26cda92b7b59fbb4f83519a7c0aa4d35eaf9eb51
fase99 <felipe.silva.e@outlook.com>
2025-12-13 03:27:58 -0300

    mejorando el front

--- app/port/page.tsx
+++ app/port/page.tsx
@@ -7,11 +7,15 @@ import Link from 'next/link';
 export const runtime = 'edge';
 
 const SAMPLE_PROJECTS = [
-  { title: 'Práctica profesional - Contraloría General de la República', description: 'Realicé escaneos a todos los segmentos de red de la institución para identificar vulnerabilidades en sistemas, equipos y configuraciones.  Identificación de vulnerabilidades de versión de las bases de datos. Además, elaboré informes de pentesting con matrices de riesgo, evaluando probabilidad e impacto para orientar la mitigación de hallazgos.', fecha: 'Dic/2024 - Feb/2025' },
   
-  { title: 'Desarrollo Fullstack - OlivoSport', description: 'Desarrollé una aplicación web para la gestión de proyectos y tareas de un centro deportivo, utilizando React para el frontend y Node.js con Express para el backend. Implementé autenticación de usuarios y almacenamiento de datos en MongoDB.', fecha: 'Mar/2023 - Jun/2023' },
+  
+  { title: 'Desarrollo Fullstack - OlivoSport', description: 'Desarrollé una aplicación web para la gestión de proyectos y tareas de un centro deportivo, utilizando React para el frontend y Node.js con Express para el backend. Implementé autenticación de usuarios y almacenamiento de datos en MongoDB.', fecha: 'Mar/2023 - Jun/2023',
+    link: 'https://github.com/fase99/OlivoSport-web',
+   },
 
-  { title: 'Desarrollo Frontend - +Arq', description: 'Desarrollé una página web para cliente de consultoría de Arquitectura +arq. Tecnologías usadas: Astro, Javascript, Typescript y CSS.', fecha: 'Jun/2025 - Sep/2025' },
+  { title: 'Desarrollo Frontend - +Arq', description: 'Desarrollé una página web para cliente de consultoría de Arquitectura +arq. Tecnologías usadas: Astro, Javascript, Typescript y CSS.', fecha: 'Jun/2025 - Sep/2025',
+    link: 'https://masarq.cl/',
+  },
 ];
 
 export default function Home() {
@@ -20,16 +24,18 @@ export default function Home() {
       <Link href="/" style={{ color: 'var(--muted)', textDecoration: 'none', position: 'absolute', top: '20px', left: '20px', zIndex: 10 }}>&#8592; Volver</Link>
       <div className="flex min-h-[100vh] h-full items-center justify-center bg-no-repeat bg-gradient-cover font-sans">
         
-        <div className="w-full" style={{  }}>
+        <div className="w-full ">
           <section className="panel  self-center" style={{ maxWidth: 980, margin: '0 auto' }}>
-            <Header title="FELIPE ALEJANDRO SILVA ESCOBAR" />
 
-            <p style={{ marginTop: '0.8rem', color: 'var(--muted)' }}>
-              Estudiante de último año de Ingeniería Civil en Informática y Telecomunicaciones. Realicé mi práctica profesional en
-Ciberseguridad en la Contraloría General de la República. Estoy en busca de un proyecto de titulación, para aplicar mis
-habilidades analíticas para resolver problemas y contribuir a soluciones robustas y eficientes en áreas como ciberseguridad,
-desarrollo de software y sistemas
-            </p>
+            <div className="flex items-center gap-8 mb-8 mt-8 justify-center ">
+
+              <img src="/sasuke-avatar.jpg" alt="Avatar" className="w-50 h-50 rounded-full object-cover border-2 border-white/10" />
+              <Header title="FELIPE ALEJANDRO SILVA ESCOBAR"  />
+              <h2 className="flex ">Software Development | Secure Infraestructure</h2>
+              
+            </div>
+
+            
 
 
             <div style={{ marginTop: '1.2rem' }}>
--- components/Expcomp.tsx
+++ components/Expcomp.tsx
@@ -1,3 +1,4 @@
+import { a } from "motion/react-client";
 import { relative } from "path";
 
 export const runtime = 'edge';
@@ -6,11 +7,14 @@ export type Exp = {
   title: string;
   description?: string;
   fecha?: string;
+  link?: string;
 };
 
-export default function Excomp({ title, description, fecha }: Exp) {
+export default function Excomp({ title, description, fecha, link }: Exp) {
   return (
     
+    <a href={link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', display: 'block' }}>
+
     <article className="panel" style={{ padding: '1rem', marginTop: '15px'}}>
       <div style={{ marginTop: '5px', textAlign: 'left' }}>
       {fecha && <p style={{ color: 'var(--muted)' }}>{fecha}</p>}
@@ -18,5 +22,6 @@ export default function Excomp({ title, description, fecha }: Exp) {
       <h3 className="neon" style={{ marginBottom: '0.4rem'}}>{title}</h3>
       {description && <p style={{ color: 'var(--muted)', marginTop: '10px'}}>{description}</p>}
     </article>
+    </a>
   );
 }
